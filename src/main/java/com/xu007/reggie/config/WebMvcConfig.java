a87afe2912585a17d2b086b0144dfef4b0140d2d
xiaoxu-007 <1599938353@qq.com>
2022-04-29 01:43:53 +0800

    项目部署

---
+++ src/main/java/com/xu007/reggie/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.xu007.reggie.config;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @author Xu007
+ * @create 2022-04-29 1:24
+ */
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始静态资源映射...");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
