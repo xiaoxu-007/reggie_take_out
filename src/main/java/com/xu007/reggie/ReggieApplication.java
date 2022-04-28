1e900b81a5bf46a64f8d532e88e34a6a81cf28a9
xiaoxu-007 <1599938353@qq.com>
2022-04-29 01:18:49 +0800

    项目部署

--- src/main/java/com/xu007/reggie/ReggieApplication.java
+++ src/main/java/com/xu007/reggie/ReggieApplication.java
@@ -1,8 +1,19 @@
 package com.xu007.reggie;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Xu007
  * @create 2022-04-29 1:12
+ *
  */
+@Slf4j
+@SpringBootApplication
 public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+        log.info("项目启动成功...");
+    }
 }
