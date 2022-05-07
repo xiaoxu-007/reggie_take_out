04bdfd60785d0e8ffc281e4fac418cb67f9939e6
xiaoxu-007 <1599938353@qq.com>
2022-05-07 18:53:20 +0800

    增加过滤器，完善登录功能

--- src/main/java/com/xu007/reggie/ReggieApplication.java
+++ src/main/java/com/xu007/reggie/ReggieApplication.java
@@ -3,6 +3,7 @@ package com.xu007.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author Xu007
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class, args);
